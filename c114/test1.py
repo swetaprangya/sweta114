07c7ad34bd41e25641fbfcbc3d78389b4598d383
swetaprangya <105152167+swetaprangya@users.noreply.github.com>
2023-05-14 15:18:41 +0530

    Add files via upload

---
+++ c114/test1.py
@@ -0,0 +1,12 @@
+a=33
+b=33
+if b>a:
+    print("b is greater than a")
+elif a==b:
+    print("a and b are equal")    
+
+def my_function(fname):
+    print(fname + "Refsnes")
+my_function("emil")
+my_function("Tobias")
+my_function("Linus")    
